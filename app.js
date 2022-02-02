b32538c9bb824a133eeef63e09047f0db4fbab51
Zoltan Csilleri <csilleri.zoltan@codebuild.hu>
2022-02-02 08:10:12 +0100

    wip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 const indexRouter = require('./routes/admin');
@@ -15,6 +16,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 const port = process.env.PORT || 3030;
-app.listen(port, () => console.log(`app is listening on port: ${port}`));
 
+console.log(process.env);
+mongoose
+    .connect(process.env.MONGO_URL,
+        {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(port, () => console.log(`app is listening on port: ${port}`));
+    })
+    .catch(err => {
+        console.log(err)
+    })
 module.exports = app;
