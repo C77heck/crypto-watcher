695f7813e8e5f3b489a92ace634893215ac2c744
Csilleri Zoltan <csillerizoltan@Pimplis-iMac-Pro-2.local>
2022-02-04 10:42:55 +0100

    wip

---
+++ models/purchased.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+const purchasedSchema = new Schema({
+    identifier: {type: Number, required: true},
+    name: {type: String, required: true},
+    price: {type: Number, required: true},
+    symbol: {type: String, required: true},
+    date: {type: Date, required: true},
+});
+
+purchasedSchema.statics.findByName = function (name) {
+    return this.where({name: new RegExp(name, 'i')});
+}
+
+purchasedSchema.plugin(uniqueValidator)
+module.exports = mongoose.model('Purchased', purchasedSchema);
